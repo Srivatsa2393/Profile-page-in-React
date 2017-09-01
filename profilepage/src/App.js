58ef0753bc7838993a5ddcef73522bc1fa0781a3
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-01 21:33:38 +0530

    updated readme

--- profilepage/src/App.js
+++ profilepage/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Image from './Image';
 import Profile from './Profile'; 
 import './App.css';
