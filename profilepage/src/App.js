c7152036806bc30003551e33ece93acd51cb96d0
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-01 20:34:46 +0530

    Added a class based App component

--- profilepage/src/App.js
+++ profilepage/src/App.js
@@ -5,11 +5,27 @@ import Profile from './Profile';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      person: {
+        name: 'Srivatsa Shankaran',
+        biography: '24 year old fullstack developer living in Bremen, Germany. Originally from Bangalore, India. Love to learn and build stuff'
+      },
+      image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAP8AAAAJDhmN2EzN2MwLTYzMGItNDIxOS05YjJlLWVjZDdhNDRhZTQ5ZQ.jpg',
+      quote: {
+        content: 'Never Ever Give Up',
+        source: 'Nature'
+      }
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Image />
-        <Profile />
+        <Image src={this.state.image} />
+        <Profile person={this.state.person} quote={this.state.quotes} />
       </div>
     );
   }
